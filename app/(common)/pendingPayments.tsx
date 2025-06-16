2da3f5a6c6929806754ea27b54ca4a53e72e9eee
vandit vasa <vasavandit@gmail.com>
2025-06-16 08:18:35 +0530

    sending category along with booking in payment endpoint

--- app/(common)/pendingPayments.tsx
+++ app/(common)/pendingPayments.tsx
@@ -170,7 +170,7 @@ const PendingPayments = () => {
   });
 
   const processPaymentMutation = useMutation({
-    mutationFn: async (paymentIds: string[]) => {
+    mutationFn: async (data: any) => {
       return new Promise((resolve, reject) => {
         handleAPICall(
           'POST',
@@ -178,7 +178,7 @@ const PendingPayments = () => {
           null,
           {
             cardno: user.cardno,
-            bookingids: paymentIds,
+            data: data,
           },
           (res: any) => {
             resolve(res);
@@ -315,8 +315,13 @@ const PendingPayments = () => {
     setShowInternationalWarning(false);
     setIsSubmitting(true);
     try {
-      const paymentIds = selectedPayments.map((payment) => payment.bookingid);
-      const result = (await processPaymentMutation.mutateAsync(paymentIds)) as any;
+      const paymentData = selectedPayments.map((payment) => {
+        return {
+          bookingid: payment.bookingid,
+          category: payment.category,
+        };
+      });
+      const result = (await processPaymentMutation.mutateAsync(paymentData)) as any;
 
       if (result.data?.amount === 0) {
         Toast.show({
