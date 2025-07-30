fad105b7f2e2861a5a886a50d0979e3f792adefa
vandit vasa <vasavandit@gmail.com>
2025-07-30 16:36:44 +0530

    fixing rendering issues in room booking page

--- components/CustomCalender.tsx
+++ components/CustomCalender.tsx
@@ -1,8 +1,7 @@
-import { useWindowDimensions } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import moment from 'moment';
+import React, { useState } from 'react';
 import { colors } from '../constants';
 import { Calendar } from 'react-native-calendars';
+import moment from 'moment';
 
 const MIN_DATE = moment(new Date()).add(1, 'days').format('YYYY-MM-DD');
 
@@ -27,59 +26,36 @@ const CustomCalender: React.FC<CustomCalenderProps> = ({
   setSelectedDay,
   minDate,
 }) => {
-  const { width } = useWindowDimensions();
   const [markedDates, setMarkedDates] = useState({});
   const [disableLeftArrow, setDisableLeftArrow] = useState(false);
 
-  // Fixed useEffect to handle both single start date and date range
-  useEffect(() => {
-    if (type === 'period') {
-      if (startDay && endDay) {
-        // Both dates selected - show full range
-        const date: any = {};
-        for (const d = moment(startDay); d.isSameOrBefore(moment(endDay)); d.add(1, 'days')) {
-          const key = d.format('YYYY-MM-DD');
-          date[key] = {
-            color: colors.orange,
-            textColor: 'white',
-          };
-
-          if (key === startDay) date[key].startingDay = true;
-          if (key === endDay) date[key].endingDay = true;
-        }
-        setMarkedDates(date);
-      } else if (startDay && !endDay) {
-        // Only start date selected - show single date
-        setMarkedDates({
-          [startDay]: {
-            color: colors.orange,
-            textColor: 'white',
-            startingDay: true,
-            endingDay: true,
-          },
-        });
-      } else {
-        // No dates selected
-        setMarkedDates({});
-      }
-    }
-  }, [startDay, endDay, type]);
-
   const handlePeriodPress = (day: any) => {
     if (startDay && !endDay) {
-      // Second click - set end date
-      if (moment(day.dateString).isBefore(moment(startDay))) {
-        // If selected date is before start date, make it the new start date
-        setStartDay(day.dateString);
-        setEndDay(null);
-      } else {
-        // Normal case - set as end date
-        setEndDay(day.dateString);
+      const date: any = {};
+      for (const d = moment(startDay); d.isSameOrBefore(day.dateString); d.add(1, 'days')) {
+        date[d.format('YYYY-MM-DD')] = {
+          color: colors.orange,
+          textColor: 'white',
+        };
+
+        if (d.format('YYYY-MM-DD') === startDay) date[d.format('YYYY-MM-DD')].startingDay = true;
+        if (d.format('YYYY-MM-DD') === day.dateString)
+          date[d.format('YYYY-MM-DD')].endingDay = true;
       }
+
+      setMarkedDates(date);
+      setEndDay(day.dateString);
     } else {
-      // First click or reset - set start date
       setStartDay(day.dateString);
       setEndDay(null);
+      setMarkedDates({
+        [day.dateString]: {
+          color: colors.orange,
+          textColor: 'white',
+          startingDay: true,
+          endingDay: true,
+        },
+      });
     }
   };
 
@@ -93,9 +69,6 @@ const CustomCalender: React.FC<CustomCalenderProps> = ({
   return (
     <Calendar
       className="mt-5"
-      style={{
-        width: width * 0.9,
-      }}
       minDate={minDate ? minDate : MIN_DATE}
       initialDate={minDate ? minDate : MIN_DATE}
       disableArrowLeft={disableLeftArrow}
