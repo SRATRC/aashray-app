783d320dfdc83e7e4fc5d5dc3dc69e134c70620c
vandit vasa <vasavandit@gmail.com>
2025-07-31 17:27:31 +0530

    dependency cleanup

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: 'Aashray',
     scheme: 'aashray',
     slug: 'aashray',
-    version: '1.1.20',
+    version: '1.1.21',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     userInterfaceStyle: 'automatic',
